0435f402ee1cfee7054abcbb890311d6c00e74b8
mepooheiei <thitipongkriklong@gmail.com>
2024-12-04 16:12:45 +0700

    Add files via upload

---
+++ 1form_15mepooh.py
@@ -0,0 +1,8 @@
+
+print("Welcome to Pre-Engineering & Robotics")
+print("\n\nสวัสดีครับ กรอกข้อมูลตนเองครับผม :) ")
+id_stu = input ("รหัสนักเรียน ")
+nickname = input("ชื่อเล่น ")
+print("\n\nยินดีต้อนรับครับ น้อง", nickname)2555
+print("รหัสนักเรียน ", id_stu) 
+print("\n\nรักษาสุขภาพ แล้วพบกันใหม่ครับ " )
